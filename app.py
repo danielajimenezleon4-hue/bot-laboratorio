819a0b75de85ceeaa0fe1202107cf5b7d330793f
Daniela <danielajimenezleon4@gmail.com>
2026-04-08 23:48:56 -0500

    fix cors

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
-
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,POST,OPTIONS')
+    return response
 pedidos = {}
 
 @app.route('/')
